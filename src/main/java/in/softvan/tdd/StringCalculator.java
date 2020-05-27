6a3e4170ec1d29265ef6f8ecef3d3f47f87b889a
Sunil Thakkar <sunil.softvan@gmail.com>
2020-05-27 16:51:16 +0530

    Initial commit

---
+++ src/main/java/in/softvan/tdd/StringCalculator.java
@@ -0,0 +1,8 @@
+package in.softvan.tdd;
+
+public class StringCalculator {
+
+  public int Add(String numbers) {
+    return 0;
+  }
+}
---
+++ src/test/java/in/softvan/tdd/tests/StringCalculatorTests.java
@@ -0,0 +1,21 @@
+package in.softvan.tdd.tests;
+
+import in.softvan.tdd.StringCalculator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class StringCalculatorTests {
+
+  private StringCalculator stringCalculator;
+
+  @BeforeEach
+  public void setUp() {
+    this.stringCalculator = new StringCalculator();
+  }
+
+  @Test
+  public void testZeroOnEmptyString() {
+    Assertions.assertEquals(0, stringCalculator.Add(""));
+  }
+}
